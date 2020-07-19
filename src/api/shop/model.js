a4f8e1f5c8c5c8de5a937c711c2b0799fc9da4c9
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-07-19 20:38:19 +0200

    Feat: WIP shop model

--- src/api/shop/model.js
+++ src/api/shop/model.js
@@ -8,15 +8,48 @@ import userAcl from 'a/user/acl'
 
 const dataSchema = new Schema(
     {
+        name: {
+            type: String,
+            required: true,
+        },
+        // TODO: how tf do we model contact?
+        contact: { },
+        description: {
+            type: String,
+            required: true // validation?
+        },
+        address: {
+            label: { type: String, required: true },
+            city: { type: String, required: true },
+            country: { type: String, required: true },
+            county: { type: String, required: true },
+            district: { type: String, required: true },
+            houseNumber: { type: String, required: true },
+            locationId: {
+                type: String,
+                required: true,
+                description: 'locationId is the only field which is actually required from the frontend, all other fields are functionally dependent and can (and should) come directly from the HERE api'
+            },
+            state: { type: String, required: true },
+            street: { type: String, required: true },
+            postalCode: { type: Number, required: true },
+        },
+        // TODO: Remove content and the corresponding tests / controller / whateve
         content: {
             type: String,
             required: true,
-            minlength: 2
+            minlength: 2,
         },
         author: {
             type: 'ObjectId',
             ref: 'User',
-            required: false
+            required: false,
+            description: 'author is the user who created the shop, not necessarily the shop owner'
+        },
+        published: {
+            type: Boolean,
+            default: false,
+            description: 'unpublished shops will not be accessible for the public (only owner+author+admin)'
         }
     },
     {
