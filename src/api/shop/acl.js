2ef8c06ce8d02a6841318c21750c58ebfeb49590
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-09-03 13:55:02 +0200

    Fix: return images when requesting shop

--- src/api/shop/acl.js
+++ src/api/shop/acl.js
@@ -6,7 +6,7 @@ const permissions = [
                 resource: 'shops/*',
                 methods: ['GET'],
                 action: 'allow',
-                view: ['name', 'slug', 'contact', 'description', 'address', 'openingHours', 'isOpen', 'author', 'author.name', 'categories']
+                view: ['name', 'slug', 'contact', 'description', 'address', 'openingHours', 'isOpen', 'author', 'author.name', 'categories', 'images']
             }
         ]
     },
@@ -17,7 +17,7 @@ const permissions = [
                 resource: 'shops/*',
                 methods: ['GET', 'POST', 'PUT', 'DELETE'],
                 action: 'allow',
-                view: ['name', 'slug', 'contact', 'description', 'address', 'author', 'published', 'openingHours', 'isOpen', 'author', 'author.name', 'author.email', 'author.picture', 'categories']
+                view: ['name', 'slug', 'contact', 'description', 'address', 'author', 'published', 'openingHours', 'isOpen', 'author', 'author.name', 'author.email', 'author.picture', 'categories', 'images']
             }
         ]
     },
@@ -28,7 +28,7 @@ const permissions = [
                 resource: 'shops/*',
                 methods: ['GET', 'POST', 'PUT', 'DELETE'],
                 action: 'allow',
-                view: ['_id', 'name', 'slug', 'contact', 'description', 'address', 'author', 'published', 'openingHours', 'isOpen', 'author', 'author.name', 'author.email', 'author.picture', 'categories']
+                view: ['_id', 'name', 'slug', 'contact', 'description', 'address', 'author', 'published', 'openingHours', 'isOpen', 'author', 'author.name', 'author.email', 'author.picture', 'categories', 'images']
             }
         ]
     }
