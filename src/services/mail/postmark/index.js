5b6943171c36047aac2cf5f2d10e68004c25d199
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-09-12 01:28:44 +0200

    Feat: Added postmark templates

--- src/services/mail/postmark/index.js
+++ src/services/mail/postmark/index.js
@@ -1,38 +1,45 @@
 import { Client } from 'postmark'
 import { postmark } from '~/config'
-import { env } from '~/config'
 
 const client = new Client(postmark.apiKey)
 
-export const sendPasswordReset = (To, name, token) => {
-    // TODO: need to get the right template first
-    return true
+const verificationLink = token => `${process.env.APP_URL}/auth/verify/${token}`
+const passwordLink = token => `${process.env.APP_URL}/password-reset/${token}`
+const loginLink = `${process.env.APP_URL}/login`
+
+export const sendPasswordReset = async (To, name, token, os, browser) => {
+
+    const { ErrorCode } = await client.sendEmailWithTemplate({
+        From: postmark.defaultEmail,
+        To,
+        TemplateAlias: 'password-reset',
+        TemplateModel: {
+            name,
+            action_url: passwordLink(token),
+            operating_system: os,
+            browser_name: browser        
+        }
+    })
+    if (ErrorCode !== 0) {
+        // log error
+    }
+    return ErrorCode === 0
 }
 
 
 export const sendVerification = async (To, name, token) => {
-    // TODO: need to get the right template first
-    return true 
-    const { ErrorCode, Message } = await client.sendEmailWithTemplate({
+    const { ErrorCode } = await client.sendEmailWithTemplate({
         From: postmark.defaultEmail,
         To,
         TemplateAlias: 'welcome',
-        TemplateModel: { // TODO:
-          product_url: 'product_url_Value',
-          product_name: 'product_name_Value',
-          name: 'name_Value',
-          action_url: 'action_url_Value',
-          login_url: 'login_url_Value',
-          username: 'username_Value',
-          trial_length: 'trial_length_Value',
-          trial_start_date: 'trial_start_date_Value',
-          trial_end_date: 'trial_end_date_Value',
-          support_email: 'support_email_Value',
-          live_chat_url: 'live_chat_url_Value',
-          sender_name: 'sender_name_Value',
-          help_url: 'help_url_Value',
-          company_name: 'company_name_Value',
-          company_address: 'company_address_Value'
+        TemplateModel: {
+            product_url: process.env.APP_URL,
+            product_name: 'Lokabees',
+            name,
+            action_url: verificationLink(token),
+            login_url: loginLink,
+            username: To,
+            support_email: 'info@lokabees.com'
         }
     })
     if (ErrorCode !== 0) {
